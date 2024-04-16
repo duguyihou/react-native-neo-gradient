2e27bb599a7a02dda7cb702ef4db3a83d3c24326
duguyihou <duguyihou@gmail.com>
2024-04-16 19:19:13 +1000

    feat: linear gradient in Android (#4)
    * feat: wip
    
    * feat: wip
    
    * feat: works
    
    * refactor: colors
    
    * refactor: wip
    
    * chore: wip
    
    * ci: disable ios build

--- android/src/main/java/com/gradient/GradientViewManager.kt
+++ android/src/main/java/com/gradient/GradientViewManager.kt
@@ -1,7 +1,16 @@
 package com.gradient
 
-import android.graphics.Color
 import android.view.View
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import com.facebook.react.bridge.ColorPropConverter
+import com.facebook.react.bridge.ReadableArray
 import com.facebook.react.uimanager.SimpleViewManager
 import com.facebook.react.uimanager.ThemedReactContext
 import com.facebook.react.uimanager.annotations.ReactProp
@@ -9,12 +18,29 @@ import com.facebook.react.uimanager.annotations.ReactProp
 class GradientViewManager : SimpleViewManager<View>() {
   override fun getName() = "GradientView"
 
+  private var colors: List<Color>? = null
+
   override fun createViewInstance(reactContext: ThemedReactContext): View {
-    return View(reactContext)
+    return  ComposeView(reactContext).apply {
+      setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+      setContent {
+        GradientView()
+      }
+    }
   }
 
-  @ReactProp(name = "color")
-  fun setColor(view: View, color: String) {
-    view.setBackgroundColor(Color.parseColor(color))
+  @Composable
+  fun GradientView() {
+    colors?.let {
+      val brush = Brush.verticalGradient(it)
+      Box(modifier = Modifier.background(brush))
+    }
+  }
+
+  @ReactProp(name = "colors")
+  fun setColors(view: View, colors: ReadableArray) {
+    this.colors = colors.toArrayList().map {
+      Color(ColorPropConverter.getColor(it, view.context))
+    }
   }
 }
